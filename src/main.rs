dcc689ec3a562ac98681ee7c04673c3c7a59da2b
kotobukid <kotobuki@daqua.jp>
2022-04-18 12:44:24 +0900

    バイナリファイルを書き出すサンプル

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::fs;
+use std::fs::File;
+use std::io::{self, Read, Write, BufReader};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+
+    fs::create_dir("./output");
+
+    let mut file = File::create("./output/f.mid")?;
+
+    // let buf = BufReader::new(io::stdin()).bytes().collect::<io::Result<Vec<u8>>>()?;
+    let buf = "hello world";
+
+    file.write_all(buf.as_bytes())?;
+    file.flush()?;
+    Ok(())
 }
