49a3728cc4fbb9ab07562b510abfddb3967cb4f9
kotobukid <kotobuki@daqua.jp>
2022-04-19 00:04:09 +0900

    拍子まで

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::fs::File;
 use std::io::{Write};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
-
     match fs::create_dir("./output") {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(_) => {}
@@ -14,7 +12,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut file = File::create("./output/f.mid")?;
 
-    let buf = "MThd";
+    let buf: &str = "MThd";
 
     file.write_all(buf.as_bytes())?;
 
@@ -47,6 +45,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     file.write_all(&track_title_length.to_be_bytes())?;
     file.write_all(&track_title.as_bytes())?;
 
+    // Tick
+    let tick: u8 = 135;  // 960 / 128
+    let tick_b: u8 = 64; // 960 % 128
+    file.write_all(&tick.to_be_bytes())?;
+    file.write_all(&tick_b.to_be_bytes())?;
+
+    // Tempo
+    file.write_all(&0_i8.to_be_bytes())?;   // 00
+    file.write_all(&255_u8.to_be_bytes())?; // FF
+    file.write_all(&81_u8.to_be_bytes())?;  // 51
+    file.write_all(&03_u8.to_be_bytes())?;  // 03   続く3バイトでテンポ情報を送る
+    file.write_all(&06_i8.to_be_bytes())?;
+    file.write_all(&138_u8.to_be_bytes())?;
+    file.write_all(&27_u8.to_be_bytes())?;
+
+    // 拍子
+    file.write_all(&0_i8.to_be_bytes())?;
+    file.write_all(&255_u8.to_be_bytes())?;
+    file.write_all(&88_u8.to_be_bytes())?;  // 58
+    file.write_all(&4_u8.to_be_bytes())?; // 04
+    file.write_all(&4_u8.to_be_bytes())?; // 04
+    file.write_all(&2_u8.to_be_bytes())?; // 02
+    file.write_all(&26_u8.to_be_bytes())?; // 18
+    file.write_all(&8_u8.to_be_bytes())?; // 08
+
+
+    // End of Track
+    let zero: i8 = 0;
+    let ff: u8 = 255;
+    let two_f: i8 = 47;
+    file.write_all(&zero.to_be_bytes())?;
+    file.write_all(&ff.to_be_bytes())?;
+    file.write_all(&two_f.to_be_bytes())?;
+    file.write_all(&zero.to_be_bytes())?;
+
     file.flush()?;
     Ok(())
 }
+
