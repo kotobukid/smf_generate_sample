0ecf16322a5c1aa4344ad720667595839ec58641
kotobukid <kotobuki@daqua.jp>
2022-04-19 10:03:13 +0900

    呼び出しパターンに応じてマクロ化

--- src/main.rs
+++ src/main.rs
@@ -80,73 +80,81 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     write!(mthd);
 
     let buf2: i32 = 6;
-    file.write_all(&buf2.to_be_bytes())?;
+    write_i16!(buf2);
 
     let file_format: i16 = 1;
     write_i16!(file_format);
-    // file.write_all(&file_format.to_be_bytes())?;
 
     let track_amount: i16 = 2;
-    // file.write_all(&track_amount.to_be_bytes())?;
     write_i16!(track_amount);
 
     let resolution: i16 = 960;
-    // file.write_all(&resolution.to_be_bytes())?;
     write_i16!(resolution);
 
     //　コンダクタートラック
 
     let start_track: &str = "MTrk";
-    file.write_all(start_track.as_bytes())?;
+    write!(start_track);
 
     let track_length: i32 = 23;
-    file.write_all(&track_length.to_be_bytes())?;
+    write_i16!(track_length);
 
-    let before_start_track_title: i16 = 255;    // 00FFxx   を表現したいが今回は分割して記述
-    file.write_all(&before_start_track_title.to_be_bytes())?;
+    let before_start_track_title: i16 = 255;
+    // 00FFxx   を表現したいが今回は分割して記述
+    write_i16!(before_start_track_title);
     let before_start_track_title2: i8 = 3;
-    file.write_all(&before_start_track_title2.to_be_bytes())?;
+    write_i16!(before_start_track_title2);
 
 
     let track_title: &str = "Track1";
     let track_title_length: i8 = track_title.as_bytes().len() as i8;
-    file.write_all(&track_title_length.to_be_bytes())?;
-    file.write_all(&track_title.as_bytes())?;
+    write_i16!(track_title_length);
+    write!(track_title);
 
     // Tick
     let tick: u8 = 135;  // 960 / 128
-    let tick_b: u8 = 64; // 960 % 128
-    file.write_all(&tick.to_be_bytes())?;
-    file.write_all(&tick_b.to_be_bytes())?;
+    let tick_b: u8 = 64;
+    // 960 % 128
+    write_i16!(tick);
+    write_i16!(tick_b);
 
     // Tempo
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&255_u8.to_be_bytes())?; // FF
-    file.write_all(&81_u8.to_be_bytes())?;  // 51
-    file.write_all(&03_u8.to_be_bytes())?;  // 03   続く3バイトでテンポ情報を送る
-    file.write_all(&06_i8.to_be_bytes())?;
-    file.write_all(&138_u8.to_be_bytes())?;
-    file.write_all(&27_u8.to_be_bytes())?;
+    write_i16!(0_i8);
+    // 00
+    write_i16!(255_u8);
+    // FF
+    write_i16!(81_u8);
+    // 51
+    write_i16!(03_u8);
+    // 03   続く3バイトでテンポ情報を送る
+    write_i16!(06_i8);
+    write_i16!(138_u8);
+    write_i16!(27_u8);
 
     // 拍子
-    file.write_all(&0_i8.to_be_bytes())?;
-    file.write_all(&255_u8.to_be_bytes())?;
-    file.write_all(&88_u8.to_be_bytes())?;  // 58
-    file.write_all(&4_u8.to_be_bytes())?; // 04
-    file.write_all(&4_u8.to_be_bytes())?; // 04
-    file.write_all(&2_u8.to_be_bytes())?; // 02
-    file.write_all(&26_u8.to_be_bytes())?; // 18
-    file.write_all(&8_u8.to_be_bytes())?; // 08
+    write_i16!(0_i8);
+    write_i16!(255_u8);
+    write_i16!(88_u8);
+    // 58
+    write_i16!(4_u8);
+    // 04
+    write_i16!(4_u8);
+    // 04
+    write_i16!(2_u8);
+    // 02
+    write_i16!(26_u8);
+    // 18
+    write_i16!(8_u8); // 08
 
 
     // End of Track
     let zero: i8 = 0;
     let ff: u8 = 255;
     let two_f: i8 = 47;
-    file.write_all(&zero.to_be_bytes())?;
-    file.write_all(&ff.to_be_bytes())?;
-    file.write_all(&two_f.to_be_bytes())?;
-    file.write_all(&zero.to_be_bytes())?;
+    write_i16!(zero);
+    write_i16!(ff);
+    write_i16!(two_f);
+    write_i16!(zero);
 
 
     // 演奏トラック
@@ -155,50 +163,69 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // トラックの長さ
     let track_length: i32 = 43;
-    file.write_all(&track_length.to_be_bytes())?;
+    write_i16!(track_length);
 
     // トラックタイトル（なし）
-    let before_start_track_title_1: i16 = 255;    // 00FFxx   を表現したいが今回は分割して記述
-    file.write_all(&before_start_track_title_1.to_be_bytes())?;
+    let before_start_track_title_1: i16 = 255;
+    // 00FFxx   を表現したいが今回は分割して記述
+    write_i16!(before_start_track_title_1);
     let before_start_track_title1_2: i8 = 3;
-    file.write_all(&before_start_track_title1_2.to_be_bytes())?;
-    file.write_all(&0_i8.to_be_bytes())?;
+    write_i16!(before_start_track_title1_2);
+    write_i16!(0_i8);
 
     // ポート
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&255_u8.to_be_bytes())?;   // ff
-    file.write_all(&33_u8.to_be_bytes())?;   // 21
-    file.write_all(&1_u8.to_be_bytes())?;   // 01
-    file.write_all(&0_u8.to_be_bytes())?;   // 00
+    write_i16!(0_i8);
+    // 00
+    write_i16!(255_u8);
+    // ff
+    write_i16!(33_u8);
+    // 21
+    write_i16!(1_u8);
+    // 01
+    write_i16!(0_u8);   // 00
 
     // リセットオールコントローラーCC#21
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&176_u8.to_be_bytes())?;   // B0 （チャンネル1）
-    file.write_all(&121_u8.to_be_bytes())?;   // 79
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
+    write_i16!(0_i8);
+    // 00
+    write_i16!(176_u8);
+    // B0 （チャンネル1）
+    write_i16!(121_u8);
+    // 79
+    write_i16!(0_i8);   // 00
 
     // バンクセレクトMSB(CC#0)
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&176_u8.to_be_bytes())?;   // B0
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
+    write_i16!(0_i8);
+    // 00
+    write_i16!(176_u8);
+    // B0
+    write_i16!(0_i8);
+    // 00
+    write_i16!(0_i8);   // 00
 
     // バンクセレクトLSB(CC#32)
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&176_u8.to_be_bytes())?;   // B0
-    file.write_all(&32_u8.to_be_bytes())?;   // 20
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
+    write_i16!(0_i8);
+    // 00
+    write_i16!(176_u8);
+    // B0
+    write_i16!(32_u8);
+    // 20
+    write_i16!(0_i8);   // 00
 
     // プログラムチェンジ
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&192_u8.to_be_bytes())?;   // C0
-    file.write_all(&41_u8.to_be_bytes())?;   // 28  1始まり
+    write_i16!(0_i8);
+    // 00
+    write_i16!(192_u8);
+    // C0
+    write_i16!(41_u8);   // 28  1始まり
 
     // ボリューム
-    file.write_all(&0_i8.to_be_bytes())?;   // 00
-    file.write_all(&176_u8.to_be_bytes())?;   // B0
-    file.write_all(&7_i8.to_be_bytes())?;   // 07
-    file.write_all(&100_u8.to_be_bytes())?;   // 64 ボリューム100
+    write_i16!(0_i8);
+    // 00
+    write_i16!(176_u8);
+    // B0
+    write_i16!(7_i8);
+    // 07
+    write_i16!(100_u8);   // 64 ボリューム100
 
     // Cを鳴らす
     write_major_on!([&0_u8], file, &60_u8);
@@ -222,10 +249,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let zero: i8 = 0;
     let ff: u8 = 255;
     let two_f: i8 = 47;
-    file.write_all(&zero.to_be_bytes())?;
-    file.write_all(&ff.to_be_bytes())?;
-    file.write_all(&two_f.to_be_bytes())?;
-    file.write_all(&zero.to_be_bytes())?;
+    write_i16!(zero);
+    write_i16!(ff);
+    write_i16!(two_f);
+    write_i16!(zero);
 
     file.flush()?;
     println!("complete");
