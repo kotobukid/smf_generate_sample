9bc9b1e9680fc5813aff3a662983ea0826c0c144
kotobukid <kotobuki@daqua.jp>
2022-04-19 10:36:18 +0900

    マクロが式パターンをリストで受け取るようにした

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     macro_rules! write_num {
         ($i:expr) => {
-            file.write_all(&$i.to_be_bytes())?;
+            println!("{}", type_of($i));
+
+            match $i {
+                // i16 => {println!("Iterator")},
+                // [u8,] => {println!("Iterator")},
+                // [i32,] => {println!("Iterator")},
+                _ => {
+                    for i in $i.iter() {
+                        file.write_all(&i.to_be_bytes())?
+                    }
+                },
+            }
+
+            // match type_of($i).chars().nth(0) {
+            //     Some('[') => for i in $i {file.write_all(&i.to_be_bytes())?;},
+            //     _ => file.write_all(&$i.to_be_bytes())?,
+            // }
+
+            // file.write_all(&$i.to_be_bytes())?;
         }
     }
 
@@ -80,16 +98,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     write!(mthd);
 
     let buf2: i32 = 6;
-    write_num!(buf2);
+    write_num!([buf2]);
 
     let file_format: i16 = 1;
-    write_num!(file_format);
+    write_num!([file_format]);
 
     let track_amount: i16 = 2;
-    write_num!(track_amount);
+    write_num!([track_amount]);
 
     let resolution: i16 = 960;
-    write_num!(resolution);
+    write_num!([resolution]);
 
     //　コンダクタートラック
 
@@ -97,135 +115,136 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     write!(start_track);
 
     let track_length: i32 = 23;
-    write_num!(track_length);
+    write_num!([track_length]);
 
     let before_start_track_title: i16 = 255;
     // 00FFxx   を表現したいが今回は分割して記述
-    write_num!(before_start_track_title);
+    write_num!([before_start_track_title]);
     let before_start_track_title2: i8 = 3;
-    write_num!(before_start_track_title2);
+    write_num!([before_start_track_title2]);
 
 
     let track_title: &str = "Track1";
     let track_title_length: i8 = track_title.as_bytes().len() as i8;
-    write_num!(track_title_length);
+    write_num!([track_title_length]);
     write!(track_title);
 
     // Tick
     let tick: u8 = 135;  // 960 / 128
     let tick_b: u8 = 64;
     // 960 % 128
-    write_num!(tick);
-    write_num!(tick_b);
+    write_num!([tick]);
+    write_num!([tick_b]);
 
     // Tempo
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(255_u8);
+    write_num!([255_u8]);
     // FF
-    write_num!(81_u8);
+    write_num!([81_u8]);
     // 51
-    write_num!(03_u8);
+    write_num!([03_u8]);
     // 03   続く3バイトでテンポ情報を送る
-    write_num!(06_i8);
-    write_num!(138_u8);
-    write_num!(27_u8);
+    write_num!([06_i8]);
+    write_num!([138_u8]);
+    write_num!([27_u8]);
 
     // 拍子
-    write_num!(0_i8);
-    write_num!(255_u8);
-    write_num!(88_u8);
+    write_num!([0_i8]);
+    write_num!([255_u8]);
+    write_num!([88_u8]);
     // 58
-    write_num!(4_u8);
+    write_num!([4_u8]);
     // 04
-    write_num!(4_u8);
+    write_num!([4_u8]);
     // 04
-    write_num!(2_u8);
+    write_num!([2_u8]);
     // 02
-    write_num!(26_u8);
+    write_num!([26_u8]);
     // 18
-    write_num!(8_u8); // 08
+    write_num!([8_u8]); // 08
 
 
     // End of Track
-    let zero: i8 = 0;
+    let zero: u8 = 0;
     let ff: u8 = 255;
-    let two_f: i8 = 47;
-    write_num!(zero);
-    write_num!(ff);
-    write_num!(two_f);
-    write_num!(zero);
+    let two_f: u8 = 47;
+    // write_num!(zero);
+    // write_num!(ff);
+    // write_num!(two_f);
+    // write_num!(zero);
+    write_num!([zero, ff, two_f, zero]);
 
 
     // 演奏トラック
     let start_track: &str = "MTrk";
-    file.write_all(start_track.as_bytes())?;
+    write!(start_track);
 
     // トラックの長さ
     let track_length: i32 = 43;
-    write_num!(track_length);
+    write_num!([track_length]);
 
     // トラックタイトル（なし）
     let before_start_track_title_1: i16 = 255;
     // 00FFxx   を表現したいが今回は分割して記述
-    write_num!(before_start_track_title_1);
+    write_num!([before_start_track_title_1]);
     let before_start_track_title1_2: i8 = 3;
-    write_num!(before_start_track_title1_2);
-    write_num!(0_i8);
+    write_num!([before_start_track_title1_2]);
+    write_num!([0_i8]);
 
     // ポート
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(255_u8);
+    write_num!([255_u8]);
     // ff
-    write_num!(33_u8);
+    write_num!([33_u8]);
     // 21
-    write_num!(1_u8);
+    write_num!([1_u8]);
     // 01
-    write_num!(0_u8);   // 00
+    write_num!([0_u8]);   // 00
 
     // リセットオールコントローラーCC#21
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(176_u8);
+    write_num!([176_u8]);
     // B0 （チャンネル1）
-    write_num!(121_u8);
+    write_num!([121_u8]);
     // 79
-    write_num!(0_i8);   // 00
+    write_num!([0_i8]);   // 00
 
     // バンクセレクトMSB(CC#0)
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(176_u8);
+    write_num!([176_u8]);
     // B0
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(0_i8);   // 00
+    write_num!([0_i8]);   // 00
 
     // バンクセレクトLSB(CC#32)
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(176_u8);
+    write_num!([176_u8]);
     // B0
-    write_num!(32_u8);
+    write_num!([32_u8]);
     // 20
-    write_num!(0_i8);   // 00
+    write_num!([0_i8]);   // 00
 
     // プログラムチェンジ
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(192_u8);
+    write_num!([192_u8]);
     // C0
-    write_num!(41_u8);   // 28  1始まり
+    write_num!([41_u8]);   // 28  1始まり
 
     // ボリューム
-    write_num!(0_i8);
+    write_num!([0_i8]);
     // 00
-    write_num!(176_u8);
+    write_num!([176_u8]);
     // B0
-    write_num!(7_i8);
+    write_num!([7_i8]);
     // 07
-    write_num!(100_u8);   // 64 ボリューム100
+    write_num!([100_u8]);   // 64 ボリューム100
 
     // Cを鳴らす
     write_major_on!([&0_u8], &60_u8);
@@ -249,10 +268,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let zero: i8 = 0;
     let ff: u8 = 255;
     let two_f: i8 = 47;
-    write_num!(zero);
-    write_num!(ff);
-    write_num!(two_f);
-    write_num!(zero);
+    write_num!([zero]);
+    write_num!([ff]);
+    write_num!([two_f]);
+    write_num!([zero]);
 
     file.flush()?;
     println!("complete");
